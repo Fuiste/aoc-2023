f9309bdaa0e83dcdf8ceeb190ab5414aa224bb23
William Pelrine <drawncloser@gmail.com>
2023-12-01 14:19:20 -0500

    Module interface

--- bin/main.ml
+++ bin/main.ml
@@ -2,4 +2,5 @@ open AOC_2023
 
 let () =
   let lines = FileIO.read_lines "./inputs/01.txt" in
-  print_endline (string_of_int (One.b lines))
+  print_endline ("1a: " ^ string_of_int (One.a lines)) ;
+  print_endline ("1b: " ^ string_of_int (One.b lines))
