4d64379b2d184440c779520e611121cd76a53ccb
William Pelrine <drawncloser@gmail.com>
2023-12-02 13:08:51 -0500

    Cleanup

--- lib/two.ml
+++ lib/two.ml
@@ -11,67 +11,63 @@ type round =
   ; b : int
   }
 
-let get_game_idx l_line =
+let idx_for l_line =
   match String.split_on_char ' ' l_line with
   | [ _; idx ] -> int_of_string idx
   | _ -> failwith "Invalid game line"
 ;;
 
-let handle_token acc token =
-  let trimmed = String.trim token in
-  match String.split_on_char ' ' trimmed with
-  | [ r; "red" ] -> { acc with r = int_of_string r }
-  | [ g; "green" ] -> { acc with g = int_of_string g }
-  | [ b; "blue" ] -> { acc with b = int_of_string b }
-  | _ -> acc
-;;
-
-let get_round r_str =
+let round_for r_str =
+  let with_token acc token =
+    let trimmed = String.trim token in
+    match String.split_on_char ' ' trimmed with
+    | [ r; "red" ] -> { acc with r = int_of_string r }
+    | [ g; "green" ] -> { acc with g = int_of_string g }
+    | [ b; "blue" ] -> { acc with b = int_of_string b }
+    | _ -> acc
+  in
   let initial_round = { r = 0; g = 0; b = 0 } in
-  String.split_on_char ',' r_str |> List.fold_left handle_token initial_round
+  String.split_on_char ',' r_str |> List.fold_left with_token initial_round
 ;;
 
-let get_rounds r_line =
+let rounds_for r_line =
   let rounds = String.split_on_char ';' r_line in
   let trimmed = List.map String.trim rounds in
-  List.map get_round trimmed
+  List.map round_for trimmed
 ;;
 
-let print_round round =
-  Printf.printf "R: %d, G: %d, B: %d\n" round.r round.g round.b
-;;
-
-let get_game idx rounds =
-  let max_r = List.fold_left (fun acc round -> max acc round.r) 0 rounds in
-  let max_g = List.fold_left (fun acc round -> max acc round.g) 0 rounds in
-  let max_b = List.fold_left (fun acc round -> max acc round.b) 0 rounds in
+let game_for idx rounds =
+  let max_for getter rounds =
+    List.fold_left (fun acc round -> max acc (getter round)) 0 rounds
+  in
+  let max_r = max_for (fun round -> round.r) rounds in
+  let max_g = max_for (fun round -> round.g) rounds in
+  let max_b = max_for (fun round -> round.b) rounds in
   { idx; max_r; max_g; max_b }
 ;;
 
 let handle_line line =
   match String.split_on_char ':' line with
   | [ l_line; r_line ] ->
-    let idx = get_game_idx l_line in
-    let rounds = get_rounds r_line in
-    get_game idx rounds
+    let idx = idx_for l_line in
+    let rounds = rounds_for r_line in
+    game_for idx rounds
   | _ -> failwith "Invalid line"
 ;;
 
-let is_possible max_r max_g max_b game =
-  game.max_r <= max_r && game.max_g <= max_g && game.max_b <= max_b
-;;
-
 let a lines =
+  let is_possible max_r max_g max_b game =
+    game.max_r <= max_r && game.max_g <= max_g && game.max_b <= max_b
+  in
   let games = List.map handle_line lines in
   let pos = is_possible 12 13 14 in
   let possible_games = List.filter pos games in
   List.fold_left (fun acc game -> acc + game.idx) 0 possible_games
 ;;
 
-let game_power game = game.max_r * game.max_g * game.max_b
-
 let b lines =
   let games = List.map handle_line lines in
+  let game_power game = game.max_r * game.max_g * game.max_b in
   let game_powers = List.map game_power games in
   List.fold_left (fun acc power -> acc + power) 0 game_powers
 ;;
