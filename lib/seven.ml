28cb88b4a646ae8290466134f9399da2262e4c42
William Pelrine <drawncloser@gmail.com>
2023-12-07 17:09:00 -0500

    lol

--- lib/seven.ml
+++ lib/seven.ml
@@ -59,24 +59,23 @@ let value_for_state state =
   | HighCard -> 1
 ;;
 
-let rec group hand groups =
-  match hand.cards with
-  | [] -> groups
-  | ca :: rest ->
-    if find_opt (fun (c, _) -> c = ca) groups = None
-    then group { hand with cards = rest } ((ca, 1) :: groups)
-    else
-      fold_left
-        (fun acc (c, i) ->
-          match c, ca with
-          | c, ca when c = ca -> (c, i + 1) :: acc
-          | _ -> (c, i) :: acc)
-        []
-        groups
-      |> group { hand with cards = rest }
-;;
-
 let state_for hand =
+  let rec group hand groups =
+    match hand.cards with
+    | [] -> groups
+    | ca :: rest ->
+      if find_opt (fun (c, _) -> c = ca) groups = None
+      then group { hand with cards = rest } ((ca, 1) :: groups)
+      else
+        fold_left
+          (fun acc (c, i) ->
+            match c, ca with
+            | c, ca when c = ca -> (c, i + 1) :: acc
+            | _ -> (c, i) :: acc)
+          []
+          groups
+        |> group { hand with cards = rest }
+  in
   let has t l = find_opt (fun n -> n = t) l != None in
   match group hand [] |> map (fun (_, n) -> n) with
   | l when l |> has 5 -> FiveKind
@@ -89,47 +88,6 @@ let state_for hand =
     TwoPair
   | l when l |> has 2 -> OnePair
   | _ -> HighCard
-;;
-
-let compare ha hb =
-  let rec comp_cards ca cb =
-    match ca, cb with
-    | [], [] -> 0
-    | ca :: _, cb :: _ when value_for ca > value_for cb -> 1
-    | ca :: _, cb :: _ when value_for ca < value_for cb -> -1
-    | _ :: rest_a, _ :: rest_b -> comp_cards rest_a rest_b
-    | _ -> failwith "Invalid hand"
-  in
-  let best hand =
-    if find_opt (fun c -> c = Joker) hand.cards != None
-    then
-      [ Ace; King; Queen; Ten; Nine; Eight; Seven; Six; Five; Four; Three; Two ]
-      |> map (fun c ->
-        { hand with
-          cards = map (fun ca -> if ca = Joker then c else ca) hand.cards
-        })
-      |> fold_left
-           (fun ha hb ->
-             if (match
-                   ( ha |> state_for |> value_for_state
-                   , hb |> state_for |> value_for_state )
-                 with
-                 | va, vb when va > vb -> 1
-                 | va, vb when va < vb -> -1
-                 | _ -> comp_cards ha.cards hb.cards)
-                >= 0
-             then ha
-             else hb)
-           hand
-    else hand
-  in
-  match
-    ( ha |> best |> state_for |> value_for_state
-    , hb |> best |> state_for |> value_for_state )
-  with
-  | va, vb when va > vb -> 1
-  | va, vb when va < vb -> -1
-  | _ -> comp_cards ha.cards hb.cards
 ;;
 
 let hands_for j_val lines =
@@ -166,6 +124,58 @@ let hands_for j_val lines =
 ;;
 
 let solve hands =
+  let compare ha hb =
+    let rec comp_cards ca cb =
+      match ca, cb with
+      | [], [] -> 0
+      | ca :: _, cb :: _ when value_for ca > value_for cb -> 1
+      | ca :: _, cb :: _ when value_for ca < value_for cb -> -1
+      | _ :: rest_a, _ :: rest_b -> comp_cards rest_a rest_b
+      | _ -> failwith "Invalid hand"
+    in
+    let best hand =
+      if find_opt (fun c -> c = Joker) hand.cards != None
+      then
+        [ Ace
+        ; King
+        ; Queen
+        ; Ten
+        ; Nine
+        ; Eight
+        ; Seven
+        ; Six
+        ; Five
+        ; Four
+        ; Three
+        ; Two
+        ]
+        |> map (fun c ->
+          { hand with
+            cards = map (fun ca -> if ca = Joker then c else ca) hand.cards
+          })
+        |> fold_left
+             (fun ha hb ->
+               if (match
+                     ( ha |> state_for |> value_for_state
+                     , hb |> state_for |> value_for_state )
+                   with
+                   | va, vb when va > vb -> 1
+                   | va, vb when va < vb -> -1
+                   | _ -> comp_cards ha.cards hb.cards)
+                  >= 0
+               then ha
+               else hb)
+             hand
+      else hand
+    in
+    match
+      ( ha |> best |> state_for |> value_for_state
+      , hb |> best |> state_for |> value_for_state )
+    with
+    | va, vb when va > vb -> 1
+    | va, vb when va < vb -> -1
+    | _ -> comp_cards ha.cards hb.cards
+  in
   hands
   |> sort compare
   |> mapi (fun i hand -> i, hand)
